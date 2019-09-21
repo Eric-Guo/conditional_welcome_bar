99d63fa54ac2fb458d720daf5d798cf6cf951d05
Eric-Guo <eric.guocz@gmail.com>
2019-09-22 07:27:09 +0800

    Few things need to make it runnings

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 ShopifyApp.configure do |config|
-  config.application_name = 'Conditional Welcome Bar'
+  config.application_name = 'eric-test'
   config.api_key = ENV['SHOPIFY_CLIENT_KEY']
   config.secret = ENV['SHOPIFY_CLIENT_SECRET']
+  config.old_secret = ""
   config.scope = ENV['SHOPIFY_API_SCOPE']
   config.embedded_app = true
+  config.api_version = "2019-07"
   config.session_repository = Shop
-  config.api_version = :unstable
   config.scripttags = [{
     event: 'onload',
     src: "#{ENV['APP_HOST_URL']}/welcome_bar.js"
